ac334bab127a062d1d9bd385549db5caea5d808d
Trong Nguyen <trong.nguyen@xappido.com>
2019-04-25 17:00:25 +0200

    init project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Google.OrTools;
+
+namespace aiso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
